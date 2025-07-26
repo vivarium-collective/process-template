4ec681e461bc733629ca3e965f861a26302d5402
Eran <agmon.eran@gmail.com>
2025-07-25 22:30:12 -0400

    update docstrings

--- {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/processes/template.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/processes/template.py
@@ -3,40 +3,89 @@ from process_bigraph.emitter import emitter_from_wires, gather_emitter_results
 
 
 class ProcessTemplate(Process):
-    '''
-    '''
+    """
+    A simple template process that scales a 'mass' value by a fixed rate
+    at each update interval. This example illustrates how to define a custom
+    Process in the process-bigraph framework.
+    """
 
     # The config_schema is a description of the information required
-    # to construct an instance of this process. 
+    # to construct an instance of this process.
     config_schema = {
         'rate': 'float'}
 
-
     def initialize(self, config):
-        '''
-        When this process is created, 
-        '''
+        """
+        Initialize the process with configuration parameters.
 
+        Parameters
+        ----------
+        config : dict
+            Configuration dictionary containing the 'rate' parameter.
+        """
         pass
 
-
     def inputs(self):
+        """
+        Define the input variables for the process.
+
+        Returns
+        -------
+        dict
+            A dictionary mapping variable names to their types.
+            This process expects one input: 'mass' of type float.
+        """
         return {
             'mass': 'float'}
-
 
     def outputs(self):
+        """
+        Define the output variables for the process.
+
+        Returns
+        -------
+        dict
+            A dictionary mapping variable names to their types.
+            This process outputs 'mass' of type float.
+        """
         return {
             'mass': 'float'}
 
-
     def update(self, state, interval):
-        delta = state['mass'] * interval * self.config['rate']
+        """
+        Compute the update to the mass based on the rate and time interval.
+
+        Parameters
+        ----------
+        state : dict
+            Dictionary containing the current state values.
+        interval : float
+            Duration over which the update occurs.
+
+        Returns
+        -------
+        dict
+            Dictionary with the computed delta in 'mass'.
+        """
+        # Extract the current mass from the input port
+        mass = state['mass']
+
+        # Calculate the change in mass based on the rate and interval
+        delta = self.config['rate'] * mass * interval
+
+        # Return the change to mass as output
         return {
             'mass': delta}
 
 
-def run_process(core, config):
+def run_process(core):
+    """
+    Run a simple simulation using the ProcessTemplate.
+    """
+
+    config = {
+        'rate': 0.1}
+
     state = {
         'mass': 1.0,
         'process': {
@@ -60,9 +109,9 @@ def run_process(core, config):
     results = gather_emitter_results(composite)[('emitter',)]
 
     assert results[-1]['time'] == duration
-    assert len(results) == 11 # ?
+    assert len(results) == 11  # 0 to 10 inclusive
 
-    print(results) # !
+    print(results)
 
 
 if __name__ == '__main__':
@@ -71,7 +120,4 @@ if __name__ == '__main__':
         'template',
         ProcessTemplate)
 
-    config = {
-        'rate': 0.1}
-
-    run_process(core, config)
+    run_process(core)
